ea4ff22fd7299313d4af2911c29b41d3867e9d48
sunbiao <1319027852@qq.com>
2014-04-21 15:37:32 +0800

    修改了线性规划例子最长Sequence的bug

--- findLongestSequence/longestSequence.cpp
+++ findLongestSequence/longestSequence.cpp
@@ -5,22 +5,25 @@ using namespace  std;
 int  longestSequence(int array[], int len, int length[]);
 int  main()
 {
-    int a[] = {5,2,8,6,3,6,9,7};
-    int l[8];
-    longestSequence(a,7,l);
+  //  int a[] = {5,2,8,6,3,6,9,7,100};
+    int  a[] = {1,2,3,4,500,6,7,8,90,1000};
+    int l[9];
+    longestSequence(a,9,l);
 }
 
 
 int  longestSequence(int array[], int len, int length[])
 {    
-     int dots[len];
+     int dots[len + 1];
+     int end = 1000;
+     dots[0] = end;
      for(int i = 0; i <= len; i++)
      {
         length[i] = 1;
      }
      for(int j = 0; j <= len; j++)
      {
-        for(int i = 0; i <= j-1; i++)
+        for(int i = 0; i <= j - 1; i++)
         {
            if(array[i] < array[j])
 	   {
@@ -29,27 +32,33 @@ int  longestSequence(int array[], int len, int length[])
 	         length[j] = length[i] + 1;
 	         dots[j] = i;
 	       }
-	       else
-	       {
-	         dots[j] = 0;
-	       }
 	   }
         }
     }
     int maxElement = length[0];
     int maxPos = 1;
-    for(int k = 0; k < len; k++)
+    cout << "指向数组的位置" << endl;
+    for(int  q = 0; q <= len; q++)
+    {
+       cout << dots[q] << endl;
+    }
+    
+    for(int k = 0; k <=  len; k++)
     {  
+	    cout<<length[k]<<" ";
        if(maxElement < length[k])	    
        {
+	      // cout<<dots[k]<<" ";
           maxElement = length[k];
 	  maxPos = k;
        }
     }
     int  shiftPos  = maxPos;
+    cout << "最后的指针为" << endl;
+    cout << shiftPos << endl;
     cout << "最长子序列为" << endl;
     cout << array[shiftPos] << endl;
-    while(dots[shiftPos] != 0)
+    while(dots[shiftPos] != end)
     {  
        cout << array[dots[shiftPos]] << endl;
        shiftPos = dots[shiftPos];
